6f74de760f0378b03c54c2d575dea8e7b0ba568d
Issac Teoh <teohissac@gmail.com>
2019-11-19 23:27:28 +0000

    added RPS logic for computer choice

--- js/script.js
+++ js/script.js
@@ -6,6 +6,27 @@
 /* global $ */
 
 
-
 // DOCUMENT READY FUNCTION BELOW
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+$( "document" ).ready(function() {
+$("#shoot").click(function() {
+  let choice = $("#input").val();
+  $("#userChoice").text(choice);
+  let computerchoice = getRandomInt(3);
 
+  console.log(computerchoice);
+  if (computerchoice === 0){
+      computerchoice.toString();
+      computerchoice = "Rock";
+  } else if (computerchoice === 1){
+      computerchoice.toString();
+      computerchoice = "Scissors";
+  }else {
+      computerchoice.toString();
+      computerchoice = "Paper";
+  }
+    
+});
+});
